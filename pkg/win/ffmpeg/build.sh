b27dffa80db2d95a3dc59122a6c10032f1981860
Jeff Moguillansky <jmoguillansky@gopro.com>
2020-12-04 15:04:20 -0800

    build: add ffmpeg build script

---
+++ pkg/win/ffmpeg/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -ex
+VCVARS64="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+cmd.exe /C "$VCVARS64" \&\& bash.exe ./pkg/win/ffmpeg/build_cmd.sh
---
+++ pkg/win/ffmpeg/build_cmd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -ex
+TMP_DIR=/mnt/c/wsltmp/gopro-pkg/external
+SCRIPT=`realpath $0`
+SCRIPT_PATH=`dirname $SCRIPT`
+
+mkdir -p $TMP_DIR/ffmpeg
+pushd $TMP_DIR/ffmpeg
+git clone -q https://git.ffmpeg.org/ffmpeg.git ffmpeg || true
+pushd ffmpeg
+./configure --target-os=win64 --arch=x86_64 --toolchain=msvc --enable-shared --disable-x86asm
+popd
+popd
