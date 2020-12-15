1f4663e4a944fbbebe31d7001f087d0cb67e2c2f
Jeff Moguillansky <jmoguillansky@gopro.com>
2020-12-15 10:22:54 -0800

    build: update sync script

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 set -xu
-OS=$1
-PKG=$2
-echo "OS: [$OS] PKG: [$PKG]"
+PKGS=$1
+echo "PKGS: [$PKGS]"
 TMP_DIR=/tmp/gopro-pkg/external
-if [ $OS == "win64" ]; then
 
+function sync {
+    PKG=$1
     if [ $PKG == "pkg-config:x64-windows" ]; then
-        mkdir -p $TMP_DIR/pkg-config && wget -nc https://github.com/jmoguillansky-gpsw/gopro-pkg/blob/master/external/win64/pkg-config.tgz?raw=true -O $TMP_DIR/pkg-config/pkg-config.tgz
-        (cd win64 && tar xzf $TMP_DIR/pkg-config/pkg-config.tgz)
-    fi
+        wget -nc https://github.com/jmoguillansky-gpsw/gopro-pkg/blob/master/external/win64/pkg-config.7z?raw=true -O $TMP_DIR/pkg-config.7z
+        (cd nodegl-env/Scripts && 7z x -y $TMP_DIR/pkg-config.7z)
 
     elif [ $PKG == "ffmpeg:x64-windows" ]; then
-        mkdir -p $TMP_DIR/ffmpeg && wget -nc https://github.com/jmoguillansky-gpsw/gopro-pkg/blob/master/external/win64/ffmpeg_x64-windows.7z?raw=true -O $TMP_DIR/ffmpeg/ffmpeg_x64-windows.7z
-        (cd win64 && 7z x $TMP_DIR/ffmpeg/ffmpeg_x64-windows.7z)
-    fi
+        wget -nc https://github.com/jmoguillansky-gpsw/gopro-pkg/blob/master/external/win64/ffmpeg_x64-windows.7z?raw=true -O $TMP_DIR/ffmpeg_x64-windows.7z
+        (cd external && 7z x -y $TMP_DIR/ffmpeg_x64-windows.7z)
+        (cd external/ffmpeg_x64-windows && python.exe scripts/install.py ..\\..\\nodegl-env)
 
     elif [ $PKG == "pthreads:x64-windows" ]; then
-        mkdir -p $TMP_DIR/pthreads && wget -nc https://github.com/jmoguillansky-gpsw/gopro-pkg/blob/master/external/win64/pthreads_x64-windows.7z?raw=true -O $TMP_DIR/pthreads/pthreads_x64-windows.7z
-        (cd win64 && 7z x $TMP_DIR/pthreads/pthreads_x64-windows.7z)
-    fi
-    
-    elif [ $PKG == "sdl2_x64-windows" ]; then
-        mkdir -p $TMP_DIR/sdl2 && wget -nc https://github.com/jmoguillansky-gpsw/gopro-pkg/blob/master/external/win64/sdl2_x64-windows.zip?raw=true -O $TMP_DIR/sdl2/sdl2_x64-windows.7z
-        (cd win64 && 7z x $TMP_DIR/sdl2/sdl2_x64-windows.7z -d sdl2_x64-windows)
-    fi
+        wget -nc https://github.com/jmoguillansky-gpsw/gopro-pkg/blob/master/external/win64/pthreads_x64-windows.7z?raw=true -O $TMP_DIR/pthreads_x64-windows.7z
+        (cd external && 7z x -y $TMP_DIR/pthreads_x64-windows.7z)
 
-fi
+    elif [ $PKG == "sdl2_x64-windows" ]; then
+        wget -nc https://github.com/jmoguillansky-gpsw/gopro-pkg/blob/master/external/win64/sdl2_x64-windows.7z?raw=true -O $TMP_DIR/sdl2_x64-windows.7z
+        (cd external && 7z x -y $TMP_DIR/sdl2_x64-windows.7z)
+    fi
+}
+
+for PKG in $PKGS
+do
+    sync $PKG
+done
