4ea5100d845523ca7c1c92a5fa3372d72283c907
Jeff Moguillansky <jmoguillansky@gopro.com>
2020-12-17 19:15:07 -0800

    build: add spirv-cross

---
+++ pkg/win/SPIRV-Cross/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+TMP_DIR=/mnt/c/wsltmp/gopro-pkg/external
+SCRIPT=`realpath $0`
+SCRIPT_PATH=`dirname $SCRIPT`
+
+mkdir -p $TMP_DIR/SPIRV-Cross
+pushd $TMP_DIR/SPIRV-Cross
+git clone git@github.com:KhronosGroup/SPIRV-Cross.git
+pushd SPIRV-Cross
+cmake.exe -H. -Bcmake-build-release -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 16 2019" && \
+cmake.exe --build cmake-build-release -j8 --config Release
+popd
+popd
